513efc2812b116058a5309d46321303c92bd4993
Shlomi Livne <shlomi@cloudius-systems.com>
2015-07-08 09:27:21 +0300

    Initalize each instance system.* tables
    Without this patch after starting a cluster cqlsh could be started
    toward 127.0.0.1 - but not toward other nodes. This fixes this issue by
    passing the ip address to enable_java_driver_xxx script.
    
    Signed-off-by: Shlomi Livne <shlomi@cloudius-systems.com>

--- resources/bin/run.sh
+++ resources/bin/run.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+i=0
+until [ ${!i} == '--options-file' ];
+do
+   let i=$i+1
+done
+let i=$i+1
+ip=`cat ${!i} | grep 'listen_address' | cut -f2 -d' '`
 exec "$1" "${@:2}" <&- &
 sleep 2
-exec $(dirname $0)/enable_drivers_single_node.py
+exec $(dirname $0)/enable_drivers_single_node.py $ip
 
 sleep 8
