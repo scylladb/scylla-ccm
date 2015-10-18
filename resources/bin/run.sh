946fd0c19a359b8b329caa9e973ac3daf07eecbd
Shlomi Livne <shlomi@cloudius-systems.com>
2015-10-18 18:17:04 +0300

    Set SCYLLA_HOME in run script
    Signed-off-by: Shlomi Livne <shlomi@cloudius-systems.com>

--- resources/bin/run.sh
+++ resources/bin/run.sh
@@ -7,6 +7,7 @@ i=0
 ip=`cat $1/conf/cassandra.yaml | grep 'listen_address' | cut -f2 -d' '`
 jmx_port=`cat $1/node.conf | grep 'jmx_port' | cut -f2 -d"'"`
 echo "" > $1/logs/system.log
+export SCYLLA_HOME $1
 exec $1/bin/scylla --options-file $1/conf/scylla.yaml "${@:2}" <&- 2>&1 | tee -a "$1/logs/system.log" &
 sleep 2
 # FIXME workaround for starting urchin-jmx - should use run script
