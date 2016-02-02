94d83448c5821c739f0573b2a8c58079f069936c
Calle Wilund <calle@scylladb.com>
2016-02-02 15:34:54 +0000

    run_jmx.sh: Using bash features, so specify bash
    Also need to escape '$' if echoing.

--- resources/bin/run_jmx.sh
+++ resources/bin/run_jmx.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/bash -x
 
 # 1 - root path of node
 # 3 - args
@@ -12,6 +12,6 @@ pkill -9 -f com.sun.management.jmxremote.port=$jmx_port || true
 sleep 2
 count=0
 tmp=/tmp/run.$$
-echo "while kill -0 `cat $SCYLLA_HOME/cassandra.pid` && [[ ($count < 10) ]] ; do let count+1 ; java -Dapiaddress=$ip -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$jmx_port -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar $SCYLLA_HOME/bin/scylla-jmx-1.0.jar <&- 2>&1 | tee -a $SCYLLA_HOME/logs/system.log.jmx; sleep 1; done ; rm $tmp" > $tmp
+echo "while kill -0 `cat $SCYLLA_HOME/cassandra.pid` && [[ (\$count < 10) ]] ; do let count+1 ; java -Dapiaddress=$ip -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$jmx_port -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar $SCYLLA_HOME/bin/scylla-jmx-1.0.jar <&- 2>&1 | tee -a $SCYLLA_HOME/logs/system.log.jmx; sleep 1; done ; rm $tmp" > $tmp
 chmod 777 $tmp
 exec $tmp &
