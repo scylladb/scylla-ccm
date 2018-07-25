5d82826a61798fd136958061288d3d4a0390c0c6
Rafi Einstein <rafie@scylladb.com>
2018-07-25 16:11:40 +0300

    Update __init__.py

--- ccmlib/utils/__init__.py
+++ ccmlib/utils/__init__.py
@@ -8,10 +8,10 @@ import sys
 class global_injector:
     def __init__(self):
         try:
-			# Python 2
+            # Python 2
             self.__dict__['builtin'] = sys.modules['__builtin__'].__dict__
         except KeyError:
-			# Python 3
+            # Python 3
             self.__dict__['builtin'] = sys.modules['builtins'].__dict__
     def __setattr__(self,name,value):
         self.builtin[name] = value
