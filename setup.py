a483e92cc1145af7149e08a908d07b9fef4fd690
Oren Efraimov <oren.efraimov@scylladb.com>
2020-10-23 00:02:57 +0300

    improvement(setup.py): Updating Setup file
    * Updating the `author` field value
    * Updating `install_requires` field and read the
       require from `requirements.txt` file
    * Creating `requirements.txt` file with following packages:
      * pyYaml==5.3.1
      * psutil==5.7.2
      * six==1.15.0
      * requests==2.24.0
      * pytest==6.1.1

--- setup.py
+++ setup.py
@@ -1,39 +1,43 @@
 #!/usr/bin/env python
-
 from os.path import abspath, join, dirname
 from platform import system
 from shutil import copyfile
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-ccmscript = 'ccm'
+ccm_script = "ccm"
 if system() == "Windows":
-    copyfile('ccm', 'ccm.py')
-    ccmscript = 'ccm.py'
+    copyfile("ccm", "ccm.py")
+    ccm_script = "ccm.py"
+
+with open("requirements.txt") as requirements_file:
+    requires = [package for package in requirements_file.readlines()]
+
+with open(abspath(join(dirname(__file__), "README.md"))) as readme_file:
+    long_description = readme_file.read()
 
 setup(
-    name='ccm',
-    version='2.0.5',
-    description='Cassandra Cluster Manager',
-    long_description=open(abspath(join(dirname(__file__), 'README.md'))).read(),
-    author='Sylvain Lebresne',
-    author_email='sylvain@datastax.com',
-    url='https://github.com/pcmanus/ccm',
-    packages=['ccmlib', 'ccmlib.cmds'],
-    scripts=[ccmscript],
-    install_requires=['pyYaml', 'psutil', 'six >=1.4.1', 'requests'],
+    name="ccm",
+    version="2.0.5",
+    description="Cassandra Cluster Manager",
+    long_description=long_description,
+    author="Balalis Shlomo, Bykov Aleksandr, Efraimov Oren, Fruchter Israel, Gelcer Fabio, Janković Aleksandar and"
+           "Kaikov Yaron",
+    package_data={"": ["*"]},
+    url="https://github.com/scylladb/scylla-ccm",
+    packages=["ccmlib", "ccmlib.cmds"],
+    scripts=[ccm_script],
+    install_requires=requires,
+    test_require=["pytest"],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4"
     ],
     include_package_data=True,
 )
