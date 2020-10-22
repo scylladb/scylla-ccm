79c8b054f10535aea750179c4b28e760432edf61
Aleksandr Bykov <alex.bykov@scylladb.com>
2020-10-22 22:38:16 +0700

    test(support_relloctable): Run tests for ccm via cli
    Added new tests which check that cluster could be configured
    via ./ccm cli command.
    
    ```
    pytest -sv tests/test_scylla_cmds.py
    pytest -sv -m reloc tests/test_scylla_cmds.py
    pytest -sv -m docker tests/test_scylla_cmds.py
    
    ```
    
    Test validate:
     - create cluster
     - create cluter with nodes
     - start cluster
     - check cluster status
     - remove cluster
    
    Also tests supported to run with pytest marks, this allow
    to run same test for relocatable packages and docker image
    
    (cherry picked from commit ed42280bdf2168c9087fbacd125a4366b9ba4aa3)

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,3 +6,5 @@ RESULTS_DIR = "test_results"
 TEST_ID = os.environ.get("CCM_TEST_ID", None)
 SCYLLA_DOCKER_IMAGE = os.environ.get(
     "SCYLLA_DOCKER_IMAGE", "scylladb/scylla-nightly:666.development-0.20201015.8068272b466")
+SCYLLA_RELOCATABLE_VERSION = os.environ.get(
+    "SCYLLA_VERSION", "unstable/master:2020-10-25T21%3A55%3A10Z")
