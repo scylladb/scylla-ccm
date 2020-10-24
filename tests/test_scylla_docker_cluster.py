e534463059896fa5d8934e32510249b5efa26df2
Fabio Gelcer <fabio@scylladb.com>
2020-10-24 15:31:38 +0300

    Added to scylla_docker_node the start scylla
    and start jmx functions (using docker cmd line).
    now, it is failing because i don't have a PID
    of my processes, so still not sure if i can just
    ignore them or not.
    
    (cherry picked from commit 23d52d4493fc94285b29fbb4b49608ca7dbe0b17)

--- tests/test_scylla_docker_cluster.py
+++ tests/test_scylla_docker_cluster.py
@@ -4,9 +4,10 @@ from unittest import TestCase
 
 class TestScyllaDockerCluster(TestCase):
     def test_01(self):
-        run(["bash", "-c", "rm -rf /home/fruch/.test/fruch01"])
+        run(["bash", "-c", "rm -rf /home/fabio/.test/fabio01"])
         from ccmlib.scylla_docker_cluster import ScyllaDockerCluster
-        cluster = ScyllaDockerCluster('/home/fruch/.test', name='fruch01', docker_image='scylladb/scylla-nightly:666.development-0.20201015.8068272b466')
+        cluster = ScyllaDockerCluster('/home/fabio/.test', name='fabio01',
+                                      docker_image='scylladb/scylla-nightly:666.development-0.20201015.8068272b466')
         cluster.populate(1)
         cluster.start(no_wait=True)
         node1 = cluster.nodes
