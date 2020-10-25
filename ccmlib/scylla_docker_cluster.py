4be4a74b4c0fe628814f4ca76c965f9dff01f9cf
Israel Fruchter <fruch@scylladb.com>
2020-10-25 12:21:27 +0200

    Move the test to pytest

--- ccmlib/scylla_docker_cluster.py
+++ ccmlib/scylla_docker_cluster.py
@@ -29,6 +29,7 @@ class ScyllaDockerNode(ScyllaNode):
         self.docker_id = None
         self.local_yaml_path = os.path.join(self.get_path(), 'conf')
         self.docker_name = f'{self.cluster.name}-{self.name}'
+        self.jmx_port = 7199
 
     def _get_directories(self):
         dirs = {}
@@ -41,20 +42,28 @@ class ScyllaDockerNode(ScyllaNode):
         return run(["docker", "ps", "-a"], stdout=PIPE).stdout.decode('utf-8').split()[-1]
 
     def update_yaml(self):
-        pass  # TODO: handle as mount point ?
+        if not os.path.exists(f'{self.local_yaml_path}/scylla.yaml'):
+            run(['bash', '-c', f'docker run --rm --entrypoint cat {self.cluster.docker_image} /etc/scylla/scylla.yaml > {self.local_yaml_path}/scylla.yaml'])
+        super(ScyllaDockerNode, self).update_yaml()
 
     def create_docker(self):
-        res = run(['bash', '-c', f"docker run -v {self.local_yaml_path}:/etc/scylla/ --name {self.docker_name} -d {self.cluster.docker_image} --smp 1"], stdout=PIPE, stderr=PIPE)
+        # TODO: handle smp correctly via the correct param/api (or only via commandline params)
+        # TODO: mount of the data dir
+        # TODO: pass down the full command line params, since the docker ones doesn't support all of them ?
+        # TODO: pass down a unique tag, with the cluster name, or id, if we have such in ccm, like test_id in SCT ?
+        self.update_yaml()
+
+        res = run(['bash', '-c', f"docker run -v {self.local_yaml_path}/scylla.yaml:/etc/scylla/scylla.yaml:ro --name {self.docker_name} -d {self.cluster.docker_image} --smp 1"], stdout=PIPE, stderr=PIPE)
         self.pid = res.stdout.decode('utf-8').strip() if res.stdout else None
         self.log_thread = DockerLogger(self,  os.path.join(self.get_path(), 'logs', 'system.log'))
         self.log_thread.start()
 
-        # TODO: to move to it's own function, since it's need to be called also for commandline when we'll have on the docker id
         # replace addresses
         network = run(['bash', '-c', f"docker inspect --format='{{{{ .NetworkSettings.IPAddress }}}}' {self.pid}"], stdout=PIPE, stderr=PIPE)
         address = network.stdout.decode('utf-8').strip() if res.stdout else None
         self.network_interfaces = {k: (address, v[1]) for k, v in self.network_interfaces.items()}
 
+        self.update_yaml()
         return res.returncode == 0, res.stdout, res.stderr
 
     def service_start(self, service_name):
@@ -81,32 +90,31 @@ class ScyllaDockerNode(ScyllaNode):
         else:
             return res.stdout.decode('utf-8').split()[1]
 
-    def start(self, *args, **kwargs):
-        # TODO: find a better place to do this trick, at initializing maybe ? only if file doesn't exist in the test cluster dir yet ? as part as `update_yaml` ?
-        # get scylla.yaml out of the docker
-        run(['bash', '-c', f'docker run --rm --entrypoint cat {self.cluster.docker_image}  /etc/scylla/scylla.yaml > {self.local_yaml_path}/scylla.yaml'])
-        # res = run([f'docker run --rm --entrypoint cat {self.cluster.docker_image}  '
-        #            f'/etc/scylla/scylla.yaml > {self.local_yaml_path}'],
-        #           stdout=PIPE, stderr=PIPE)
-        # if not res.returncode:
-        #     print(f'Failed to copy scylla.yaml to {self.local_yaml_path}\n{res.stderr}')
-        return super(ScyllaDockerNode, self).start(*args, **kwargs)
+    # def start(self, *args, **kwargs):
+    #    return super(ScyllaDockerNode, self).start(*args, **kwargs)
 
     def _start_scylla(self, args, marks, update_pid, wait_other_notice,
                       wait_for_binary_proto, ext_env):
-        # TODO: handle smp correctly via the correct param/api (or only via commandline params)
-        # TODO: mount of the data dir
-        # TODO: pass down the full command line params, since the docker ones doesn't support all of them ?
-        # TODO: pass down a unique tag, with the cluster name, or id, if we have such in ccm, like test_id in SCT ?
-
         rc, out, err = self.create_docker()
         if not rc:
             raise BaseException(f'failed to create docker {self.docker_name}')
 
-        time.sleep(5)
+        time.sleep(2)
         scylla_status = self.service_status('scylla')
         if scylla_status and scylla_status.upper() != 'RUNNING':
             self.service_start('scylla')
+
+        if wait_other_notice:
+            for node, mark in marks:
+                node.watch_log_for_alive(self, from_mark=mark)
+
+        if wait_for_binary_proto:
+            try:
+                self.wait_for_binary_interface(from_mark=self.mark, process=self._process_scylla, timeout=420)
+            except TimeoutError as e:
+                if not self.wait_for_bootstrap_repair(from_mark=self.mark):
+                    raise e
+                pass
 
     def do_stop(self, gently=True):
         """
