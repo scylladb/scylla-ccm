6d07dd0d4297c5f541da03407d123db0c839a875
Israel Fruchter <fruch@scylladb.com>
2020-11-08 22:59:50 +0200

    pin `six >=1.12.0`, so use `six.ensure_text()`

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     url='https://github.com/pcmanus/ccm',
     packages=['ccmlib', 'ccmlib.cmds'],
     scripts=[ccmscript],
-    install_requires=['pyYaml', 'psutil', 'six >=1.4.1', 'requests', 'packaging', 'boto3'],
+    install_requires=['pyYaml', 'psutil', 'six >=1.12.0', 'requests', 'packaging', 'boto3'],
     tests_require=['pytest'],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
