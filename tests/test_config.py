ee125560d117182c28bad4db074e36482fb16aee
Israel Fruchter <fruch@scylladb.com>
2021-01-06 09:04:08 +0200

    github action: fix the cache to actully work

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,3 +8,5 @@ SCYLLA_DOCKER_IMAGE = os.environ.get(
     "SCYLLA_DOCKER_IMAGE", "scylladb/scylla-nightly:666.development-0.20201015.8068272b466")
 SCYLLA_RELOCATABLE_VERSION = os.environ.get(
     "SCYLLA_VERSION", "unstable/master:2020-08-29T22:24:05Z")
+
+# Jan/6 comment to refresh the action cache
