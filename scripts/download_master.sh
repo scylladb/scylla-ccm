0e2d4b12e5c5b4173a315b40c23bfc9040bb1eed
Israel Fruchter <fruch@scylladb.com>
2021-01-06 12:20:46 +0200

    feature(helper_scripts): script to download branched version and RCs
    since recently some of the folder structure in our S3 for relocatable packages
    changed, we are introducting helper script for whom need quickly get those versions
    
    Example of taking a release canidate of 4.3 branch:
    
    [![asciicast](https://asciinema.org/a/382888.svg)](https://asciinema.org/a/382888)

---
+++ scripts/download_master.sh
@@ -0,0 +1,28 @@
+# download enterprise latest relocatable from enterprise branch
+#
+# Usage:
+#   ./scripts/download_enterprise.sh
+
+export BRANCH=master
+
+export LATEST_MASTER_JOB_ID=`aws --no-sign-request s3 ls downloads.scylladb.com/unstable/scylla/${BRANCH}/relocatable/ | grep '-' | tr -s ' ' | cut -d ' ' -f 3 | tr -d '\/'  | sort -g | tail -n 1`
+AWS_BASE=s3://downloads.scylladb.com/unstable/scylla/${BRANCH}/relocatable/${LATEST_MASTER_JOB_ID}
+
+rm scylla-*.tar.gz
+
+aws s3 --no-sign-request cp ${AWS_BASE}/scylla-package.tar.gz .
+aws s3 --no-sign-request cp ${AWS_BASE}/scylla-tools-package.tar.gz .
+aws s3 --no-sign-request cp ${AWS_BASE}/scylla-jmx-package.tar.gz .
+
+NAME=master_$LATEST_MASTER_JOB_ID
+NAME=$(echo master_$LATEST_MASTER_JOB_ID | sed 's/:/_/g')
+
+ccm create scylla-driver-temp -n 1 --scylla --version $NAME \
+  --scylla-core-package-uri=./scylla-package.tar.gz \
+  --scylla-tools-java-package-uri=./scylla-tools-package.tar.gz \
+  --scylla-jmx-package-uri=./scylla-jmx-package.tar.gz
+
+ccm remove
+
+echo "now it can be used in dtest as:"
+echo "export SCYLLA_VERSION=$NAME"
