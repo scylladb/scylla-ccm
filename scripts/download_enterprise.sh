de468aa8c22e4a53207ba22cc7491b9fcf078a75
Israel Fruchter <fruch@scylladb.com>
2021-11-23 21:29:16 +0200

    scripts/download_enterprise: download version from new S3 location
    location of the relocatable packages tree move to a diffrent location

--- scripts/download_enterprise.sh
+++ scripts/download_enterprise.sh
@@ -3,8 +3,10 @@
 # Usage:
 #   ./scripts/download_enterprise.sh
 
-LATEST_ENTERPRISE_JOB_ID=`aws --no-sign-request s3 ls downloads.scylladb.com/enterprise/relocatable/unstable/enterprise/ | grep '-' | tr -s ' ' | cut -d ' ' -f 3 | tr -d '\/'  | sort -g | tail -n 1`
+BRANCH=enterprise
+LATEST_ENTERPRISE_JOB_ID=`aws --no-sign-request s3 ls downloads.scylladb.com/unstable/scylla-enterprise/${BRANCH}/relocatable/ | grep '-' | tr -s ' ' | cut -d ' ' -f 3 | tr -d '\/'  | sort -g | tail -n 1`
 AWS_BASE=s3://downloads.scylladb.com/enterprise/relocatable/unstable/enterprise/${LATEST_ENTERPRISE_JOB_ID}
+AWS_BASE=s3://downloads.scylladb.com/unstable/scylla-enterprise/${BRANCH}/relocatable/${LATEST_ENTERPRISE_JOB_ID}
 
 rm scylla-*.tar.gz
 
@@ -13,12 +15,12 @@ aws s3 --no-sign-request cp ${AWS_BASE}/scylla-enterprise-tools-package.tar.gz .
 aws s3 --no-sign-request cp ${AWS_BASE}/scylla-enterprise-jmx-package.tar.gz .
 
 NAME=master_$LATEST_ENTERPRISE_JOB_ID
-NAME=$(echo master_$LATEST_ENTERPRISE_JOB_ID | sed 's/:/_/g')
+NAME=$(echo enterprise_$LATEST_ENTERPRISE_JOB_ID | sed 's/:/_/g')
 
 ccm create scylla-driver-temp -n 1 --scylla --version $NAME \
-  --scylla-core-package-uri=./scylla-package.tar.gz \
-  --scylla-tools-java-package-uri=./scylla-tools-package.tar.gz \
-  --scylla-jmx-package-uri=./scylla-jmx-package.tar.gz
+  --scylla-core-package-uri=./scylla-enterprise-package.tar.gz \
+  --scylla-tools-java-package-uri=./scylla-enterprise-tools-package.tar.gz \
+  --scylla-jmx-package-uri=./scylla-enterprise-jmx-package.tar.gz
 
 ccm remove
 
