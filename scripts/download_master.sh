c2fa4335b75ae9b78014b14d3269720570d24caa
Israel Fruchter <fruch@scylladb.com>
2021-12-28 14:28:36 +0200

    scripts/*: switch to let ccm download files
    since the move to have the arch in the package name, the ccm code
    is going to do a better job of selecting the correct packages to download

--- scripts/download_master.sh
+++ scripts/download_master.sh
@@ -8,20 +8,9 @@ export BRANCH=master
 export LATEST_MASTER_JOB_ID=`aws --no-sign-request s3 ls downloads.scylladb.com/unstable/scylla/${BRANCH}/relocatable/ | grep '-' | tr -s ' ' | cut -d ' ' -f 3 | tr -d '\/'  | sort -g | tail -n 1`
 AWS_BASE=s3://downloads.scylladb.com/unstable/scylla/${BRANCH}/relocatable/${LATEST_MASTER_JOB_ID}
 
-rm scylla-*.tar.gz
+NAME="unstable/master:$LATEST_MASTER_JOB_ID"
 
-aws s3 --no-sign-request cp ${AWS_BASE}/scylla-x86_64-package.tar.gz .
-aws s3 --no-sign-request cp ${AWS_BASE}/scylla-tools-package.tar.gz .
-aws s3 --no-sign-request cp ${AWS_BASE}/scylla-jmx-package.tar.gz .
-
-NAME=master_$LATEST_MASTER_JOB_ID
-NAME=$(echo master_$LATEST_MASTER_JOB_ID | sed 's/:/_/g')
-
-ccm create scylla-driver-temp -n 1 --scylla --version $NAME \
-  --scylla-core-package-uri=./scylla-x86_64-package.tar.gz \
-  --scylla-tools-java-package-uri=./scylla-tools-package.tar.gz \
-  --scylla-jmx-package-uri=./scylla-jmx-package.tar.gz
-
+ccm create scylla-driver-temp -n 1 --scylla --version $NAME
 ccm remove
 
 echo "now it can be used in dtest as:"
