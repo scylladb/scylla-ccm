scylladb/scylla-ccm

commit 6771eb09e3a914b465ec4c13ca23319d7b395126

scylla_node: start: add replace_node parameter
To be used for replacing a node given its host_id
rather than its ip address.

Makes use of the newly added --replace-node-first-boot
scylla option.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

ccmlib/scylla_node.py
@@ -309,10 +309,10 @@ class ScyllaNode(Node):
         if wait_for_binary_proto:
             try:
                 t = timeout * 4 if timeout is not None else 420 if self.cluster.scylla_mode != 'debug' else 900
-                self.wait_for_binary_interface(from_mark=self.mark, process=self._process_scylla, timeout=t)
+                self.wait_for_binary_interface(from_mark=self.mark, process=self._process_scylla, timeout=60)
             except TimeoutError as e:
-                if not self.wait_for_starting(from_mark=self.mark):
-                    raise e
+                if not self.wait_for_starting(from_mark=self.mark, timeout=t):
+                    raise NodeError(f"{e}")
                 pass
 
         return self._process_scylla
@@ -438,7 +438,7 @@ class ScyllaNode(Node):
     # Scylla Overload start
     def start(self, join_ring=True, no_wait=False, verbose=False,
               update_pid=True, wait_other_notice=None, replace_token=None,
-              replace_address=None, jvm_args=None, wait_for_binary_proto=None,
+              replace_address=None, replace_node_host_id=None, jvm_args=None, wait_for_binary_proto=None,
               profile_options=None, use_jna=False, quiet_start=False):
         """
         Start the node. Options includes:
@@ -451,8 +451,11 @@ class ScyllaNode(Node):
             have marked this node UP.
           - replace_token: start the node with the -Dcassandra.replace_token
             option.
-          - replace_address: start the node with the
-            -Dcassandra.replace_address option.
+          - replace_node_host_id: start the node with the
+            --replace-node-first-boot option to replace a given node
+            identified by its host_id.
+          - replace_address: start the node with the deprecated
+            --replace-address option.
 
         Extra command line options may be passed using the
         SCYLLA_EXT_OPTS environment variable.
@@ -585,7 +588,10 @@ class ScyllaNode(Node):
             args += ['--overprovisioned']
         if '--prometheus-address' not in args:
             args += ['--prometheus-address', data['api_address']]
-        if replace_address:
+        if replace_node_host_id:
+            assert replace_address is None, "replace_node_host_id and replace_address cannot be specified together"
+            args += ['--replace-node-first-boot', replace_node_host_id]
+        elif replace_address:
             args += ['--replace-address', replace_address]
         args += ['--unsafe-bypass-fsync', '1']
 

commit 765190f56c2bb28c1c1f27915e9fdf6dfcabcf09

scylla_node: start_scylla: adjust timeout for wait_for_binary_interface
As seen in e.g. https://jenkins.scylladb.com/view/master/job/scylla-master/job/dtest-daily-debug/135/testReport/resharding_test/TestReshardingVariants/Run_Dtest_Parallel_Cloud_Machines___HeavyDtest___heavy_split002___test_resharding_by_murmur3_decrease_4_TimeWindowCompactionStrategy_15_/
60 seconds are a tad too short for a node to start normally in debug mode.

That broke in 6771eb09e3a914b465ec4c13ca23319d7b395126
that passed timeout=60 instead of timeout=t.

Since wait_for_starting is a superset of wait_for_binary_interface
by exteding the wait time while the node bootstraps
we don't have to spend too much time waiting in
wait_for_binary_interface.
180 seconds would suffice for most cases.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

ccmlib/scylla_node.py
@@ -311,7 +311,7 @@ class ScyllaNode(Node):
         if wait_for_binary_proto:
             t = timeout * 4 if timeout is not None else 420 if self.cluster.scylla_mode != 'debug' else 900
             try:
-                self.wait_for_binary_interface(from_mark=self.mark, process=self._process_scylla, timeout=60)
+                self.wait_for_binary_interface(from_mark=self.mark, process=self._process_scylla, timeout=timeout or 180)
             except TimeoutError as e:
                 if not self.wait_for_starting(from_mark=self.mark, timeout=t):
                     raise NodeError(f"{e}")