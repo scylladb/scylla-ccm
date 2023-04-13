b82c996a4af5ae3d97c09781040193e9c3fc2b02
Israel Fruchter <fruch@scylladb.com>
2023-04-13 14:57:00 +0300

    tests: update to use recent scylla version
    we didn't replace the scylla under test in CCM ci
    for more then two years now. it's about time we did.

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,6 +7,6 @@ TEST_ID = os.environ.get("CCM_TEST_ID", None)
 SCYLLA_DOCKER_IMAGE = os.environ.get(
     "SCYLLA_DOCKER_IMAGE", "scylladb/scylla-nightly:666.development-0.20201015.8068272b466")
 SCYLLA_RELOCATABLE_VERSION = os.environ.get(
-    "SCYLLA_VERSION", "unstable/master:2021-11-18T04:00:29Z")
+    "SCYLLA_VERSION", "unstable/master:2023-04-13T13:31:00Z")
 
 # Feb/8 comment to refresh the action cache
