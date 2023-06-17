e721a5b92368a2bda094796cfab2230f0c3d110b
Karol Baryła <karol.baryla@scylladb.com>
2023-06-17 02:43:23 +0200

    Use advisory locks for lockfiles
    Up until now the locking mechanism used to prevent concurrent
    downloads was just the existence of a lock file. This implementation
    had several problems:
     - Locking procedure was unsafe - it was possible for more than
       one process to enter critical section at the same time. This
       was "fixed" by putting random sleep in front of it, but that is
       not a proper synchronisation mechanism.
     - Download procedure was clearing whole directory at one point.
       This caused lockfile to be removed too, so it was possible for
       another process to enter the critical section at this point.
     - If the process was killed unexpectedly, the lockfile remained in
       place. If you try to start downloading again it will wait for an hour
       for the lockfile to be deleted. This is frustrating when running e.g.
       tests for some driver, as they just hang and you have no idea why.
    
    This commit replaces that with advisory locks and fixes all those problems.
    Now the download procedure will just restart if it was aborted previously.
    It's no longer possible for 2 processes to enter critical section, so the
    (0-5) seconds sleep is removed. Download procedure no longer removes the lockfile.

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,4 +1,6 @@
-from ccmlib.common import scylla_extract_mode
+from ccmlib.common import scylla_extract_mode, LockFile
+import tempfile
+import os
 
 
 def test_scylla_extract_mode():
@@ -23,3 +25,44 @@ def test_scylla_extract_mode():
                                '30ce52b2e.tar.gz') == 'release'
     assert scylla_extract_mode('url=https://s3.amazonaws.com/downloads.scylladb.com/downloads/scylla-enterprise/'
                                'relocatable/scylladb-2022.1/scylla-enterprise-debug-aarch64-package-2022.1.rc0.0.20220331.f3ee71fba.tar.gz') == 'debug'
+
+
+# Those tests assume that LockFile uses fcntl.flock
+# If it switches to anything else, the tests need to be adjusted.
+
+def test_lockfile_basic():
+    f, path = tempfile.mkstemp(prefix='ccm-test-lockfile')
+    lf = LockFile(path)
+    assert lf.acquire(blocking=True) == (True, None)
+
+    assert lf.read_contents() == (os.getpid(), '')
+    lf.write_status('abc')
+    assert lf.read_contents() == (os.getpid(), 'abc')
+
+    lf.release()
+
+
+def test_lockfile_locks():
+    f, path = tempfile.mkstemp(prefix='ccm-test-lockfile')
+    lf1 = LockFile(path)
+    lf2 = LockFile(path)
+    with lf1:
+        assert lf2.acquire(blocking=False) == (False, os.getpid())
+    assert lf2.acquire(blocking=False) == (True, None)
+    assert lf1.acquire(blocking=False) == (False, os.getpid())
+    lf2.release()
+
+
+def test_lockfile_retain_status_by_default():
+    f, path = tempfile.mkstemp(prefix='ccm-test-lockfile')
+    lf = LockFile(path)
+
+    assert lf.acquire(blocking=False)[0] is True
+    lf.write_status('some_status_1')
+    assert lf.read_status() == 'some_status_1'
+    lf.release()
+
+    # Status should be retained from previous lock.
+    assert lf.acquire(blocking=False)[0] is True
+    assert lf.read_status() == 'some_status_1'
+    lf.release()
