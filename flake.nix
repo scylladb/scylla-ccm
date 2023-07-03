31b6942b80cd0b5045f026cc4188397c01feaa2f
Karol Baryła <karol.baryla@scylladb.com>
2023-07-03 13:48:19 +0200

    Add Nix flake
    This commit adds Nix flake which can be used to add ccm as a dependency
    in other Nix projects or to quickly enter development environment with
    all dependencies required to run and test ccm.

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A script/library to create, launch and remove an Scylla / Apache Cassandra cluster on
+localhost.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      prepare_python_requirements = python: python.withPackages (ps: with ps; [ pytest pyyaml psutil six requests packaging boto3 tqdm setuptools ]);
+      make_ccm_package = {python, jdk, pkgs}: python.pkgs.buildPythonApplication {
+        pname = "scylla_ccm";
+        version = "0.1";
+
+        src = ./. ;
+
+        checkInputs = with python.pkgs; [ pytestCheckHook ];
+        buildInputs = [ pkgs.makeWrapper ];
+        propagatedBuildInputs =  [ (prepare_python_requirements python) jdk];
+        doCheck = false;
+
+        disabledTestPaths = [ "old_tests/*.py" ];
+
+        # Make `nix run` aware that the binary is called `ccm`.
+        meta.mainProgram = "ccm";
+
+        postInstall = ''
+          wrapProgram $out/bin/ccm \
+            --set JAVA_HOME "${jdk}"
+        '';
+      };
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        packages = rec {
+          scylla_ccm = python: make_ccm_package { inherit python pkgs ; jdk = pkgs.jdk8; };
+          default = scylla_ccm pkgs.python311;
+        };
+        devShell =
+          pkgs.mkShell {
+            buildInputs = [ pkgs.poetry pkgs.glibcLocales (prepare_python_requirements pkgs.python39) (prepare_python_requirements pkgs.python311) pkgs.jdk8];
+            shellHook = ''
+              set JAVA_HOME ${pkgs.jdk8}
+            '';
+        };
+      }
+    ) // rec {
+      overlays.default = final: prev: {
+        scylla_ccm = python: make_ccm_package { inherit python; jdk = final.jdk8; pkgs = final; };
+      };
+    };
+}
