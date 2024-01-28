bdf4ea158655d05390a4979f477b48d6ba0fa0a6
Julia Yakovlev <julia.yakovlev@scylladb.com>
2024-01-28 12:41:34 +0200

    improvement(setup): improve caching of local tarballs
    If the test version is unstable (not release, maybe private branch) and installation folder exists,
    we want to check if this version was downloaded nd installed already in the past and was
    changed.
    In this case the version should be downloaded and installed again.
    Compare hash of saved version (it is saved in the 'scylla-core-package/source.txt' file) and hash
    of a new package.
    If it is same - skip the download. If not - remove existing folder and download again.

--- tests/test_scylla_download.py
+++ tests/test_scylla_download.py
@@ -3,7 +3,7 @@ import pathlib
 import pytest
 import requests
 
-from ccmlib.utils.download import download_file, download_version_from_s3
+from ccmlib.utils.download import download_file, download_version_from_s3, get_url_hash
 
 
 @pytest.mark.repo_tests
@@ -29,3 +29,8 @@ class TestUtilsDownload:
         with pytest.raises(requests.exceptions.HTTPError, match='Not Found'):
             download_file("https://s3.amazonaws.com/downloads.scylladb.com/abcdefg",
                           target_path=pathlib.Path(tmpdir) / 'scylla-manager.repo')
+
+    def test_get_local_tarball_hash(self):
+        this_path = pathlib.Path(__file__).parent
+        url_hash = get_url_hash(url=str(this_path / "tests" / "test_data" / "scylla_unified_master_2023_04_03.tar.gz"))
+        assert url_hash == 'd2be7852b8c65f74c1da8c9efbc7e408'
