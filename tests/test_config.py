6f4fc789c1581108823226738e708b7d2a1cad8f
Botond Dénes <bdenes@scylladb.com>
2024-03-22 04:51:15 -0400

    tests/test_config.py: update relocatable scylla version
    To a version which has a fully feature complete native nodetool. In the
    next commits, we will switch `scylla-node.py` to use the native nodetool
    directly, so a sufficiently recent package version is needed, for the
    tests to keep working.

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,6 +6,6 @@ TEST_ID = os.environ.get("CCM_TEST_ID", None)
 SCYLLA_DOCKER_IMAGE = os.environ.get(
     "SCYLLA_DOCKER_IMAGE", "scylladb/scylla-nightly:666.development-0.20201015.8068272b466")
 SCYLLA_RELOCATABLE_VERSION = os.environ.get(
-    "SCYLLA_VERSION", "unstable/master:2023-04-13T13:31:00Z")
+    "SCYLLA_VERSION", "unstable/master:2024-03-22T09:51:51Z")
 
 # Feb/8 comment to refresh the action cache
