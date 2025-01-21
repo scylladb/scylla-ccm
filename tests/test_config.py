71e8a3a9c21430eb84d63c5439ae6e7d213bf975
Petr Hála <petr.hala@scylladb.com>
2025-01-21 11:47:19 +0100

    fix(test_config): Do not get version from env
    - Tests expect the exact version and if the SCYLLA_VERSION is set, it will fail

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,6 @@ RESULTS_DIR = "test_results"
 TEST_ID = os.environ.get("CCM_TEST_ID", None)
 SCYLLA_DOCKER_IMAGE = os.environ.get(
     "SCYLLA_DOCKER_IMAGE", "scylladb/scylla-nightly:666.development-0.20201015.8068272b466")
-SCYLLA_RELOCATABLE_VERSION = os.environ.get(
-    "SCYLLA_VERSION", "release:2024.2.3")
+SCYLLA_RELOCATABLE_VERSION = "release:2024.2.3"
 
 # Feb/8 comment to refresh the action cache
