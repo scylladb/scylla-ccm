d1b4d46700404e42e9b723096a6c37d91e838d59
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-04-24 01:15:31 -0400

    Make hostid return None if nodetool failed

--- ccmlib/node.py
+++ ccmlib/node.py
@@ -1439,15 +1439,18 @@ class Node(object):
 
     def hostid(self, timeout=60, force_refresh=False):
         if not hasattr(self, 'node_hostid') or force_refresh:
-            info = self.nodetool('info', capture_output=True, timeout=timeout)[0]
-            id_lines = [s for s in info.split('\n')
-                        if s.startswith('ID')]
-            if not len(id_lines) == 1:
-                msg = ('Expected output from `nodetool info` to contain exactly 1 '
-                    'line starting with "ID". Found:\n') + info
-                raise RuntimeError(msg)
-            id_line = id_lines[0].replace(":", "").split()
-            self.node_hostid = id_line[1]
+            try:
+                info = self.nodetool('info', capture_output=True, timeout=timeout)[0]
+                id_lines = [s for s in info.split('\n')
+                            if s.startswith('ID')]
+                if not len(id_lines) == 1:
+                    msg = ('Expected output from `nodetool info` to contain exactly 1 '
+                        'line starting with "ID". Found:\n') + info
+                    raise RuntimeError(msg)
+                id_line = id_lines[0].replace(":", "").split()
+                self.node_hostid = id_line[1]
+            except Exception as e:
+                self.error(f"Failed to get hostid via nodetool: {e}")
         return self.node_hostid
 
     def get_datacenter_name(self):
